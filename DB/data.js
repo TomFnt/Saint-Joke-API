1962133d51e8ea21c67dc91ad3e3426ca517a1d2
Tom Fontaine <tom.fnt9@gmail.com>
2023-06-19 14:29:35 +0200

    First commit

---
+++ DB/data.js
@@ -0,0 +1,42 @@
+const joke = [
+    {
+        "joke" : "1 : Quel est la femelle du hamster ?",
+        "response":"l'Amsterdam"
+    },
+    {
+        "joke" : "2 : Que dit un oignon quand il se cogne ?",
+        "response":"Aïe"
+    },
+    {
+        "joke" : "3 : Quel est l'animal le plus heureux ?",
+        "response":"le hiboux, parce que sa femme est chouette ! "
+    },
+    {
+        "joke" : "4 : Pourqoi le football c'est rigolo ?",
+        "response":"Parce que Thierry en rit"
+    },
+    {
+        "joke" : "5 : Quel est le sport le plus fruité ?",
+        "response":"la boxe, parce que tu te prends des pêches dans la poire et tu tombes dans les pommes."
+    },
+    {
+        "joke" : "6 : Que ce fait un Schtroumpf quand il tombe ?",
+        "response":"un Bleu"
+    },
+    {
+        "joke" : "7 : Quel est le comble pour un marin ? ",
+        "response":"Avoir le nez qui coule !"
+    },
+    {
+        "joke" : " 8 : Qu'est ce que les enfants usent le plus à l'école ? ",
+        "response":"Le professeur ! "
+    },
+    {
+        "joke" : " 9 : Quel est le sport le plus silencieux ",
+        "response":" Le para-chuuuuuut..."
+    },
+    {
+        "joke" : " 10 : Quel est le comble pour un joueur de bowling ? ",
+        "response":"Avoir le nez qui coule !"
+    }
+]
